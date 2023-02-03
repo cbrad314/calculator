919221fdfbc7d317aa64eb279a0cc3022a840715
Chris <chris.p.bradford@gmail.com>
2023-02-02 18:04:26 -0600

    complete functional calculator sans decimals

--- calculator.js
+++ calculator.js
@@ -3,27 +3,59 @@ let firstValue = null;
 let secondValue = null;
 let operatorValue = null;
 let calculatedValue;
-const display = document.querySelector('.display')
+let lastOperator;
+const display = document.querySelector('#display ')
+const aggregatedDisplay = document.querySelector('#aggregator')
+const aggregator = [];
 const allButtons = document.querySelectorAll('button')
 const allNumberButtons = document.querySelectorAll('.number')
 const allOperatorButtons = document.querySelectorAll('.operator')
 const clear = document.querySelector('.clear')
-const compute = document.querySelector('.compute')
+const compute = document.querySelector('#compute')
 
-function updateDisplay(text) {
-  display.textContent = text;
+
+function updateDisplay(displayValue) {
+  display.textContent = displayValue;
+}
+
+function updateDisplays(displayValue,aggregatorValue,type){
+  updateDisplay(displayValue);
+  updateAggregator(aggregatorValue,type);
+}
+function updateAggregator(aggregatorValue, type) {
+  if (!type||type ==='push') {aggregator.push(aggregatorValue);}
+  else if (type==='clear') {aggregator.splice(0,aggregator.length);}
+  else if (type==='change') {aggregator.splice(aggregator.length-1,1,aggregatorValue);}
+  else if (type==='error') {aggregator.splice(0,aggregator.length,aggregatorValue);}
+  updateAggregatedDisplay(aggregator);
+}
+
+function updateAggregatedDisplay(text){
+  aggregatedDisplay.textContent = aggregator.join(' ');
 }
 
 allNumberButtons.forEach(button => {button.addEventListener('click',updateOperands)});
 
-allOperatorButtons.forEach(button => button.addEventListener('click',storeOperator));
+allOperatorButtons.forEach(button => button.addEventListener('click',(event) => lastOperator = event.target.textContent));
+allOperatorButtons.forEach(button => button.addEventListener('click',runOperator));
+
 clear.addEventListener('click', clearMemory);
-compute.addEventListener('click',computeValues);
+// compute.addEventListener('click',computeValues);
+
 
 function computeValues(){
-  if (firstValue&&secondValue&&operatorValue)
-    operate(firstValue,secondValue,operatorValue)
-    updateDisplay(calculatedValue);
+  if (firstValue&&secondValue&&operatorValue){
+    operate(firstValue,secondValue,operatorValue);
+    if (lastOperator === '='){
+      updateDisplays(calculatedValue,'= ' + calculatedValue,'push')
+    }
+    else if (lastOperator !== '=') {updateDisplays(calculatedValue, lastOperator,'push');}
+    firstValue = calculatedValue;
+    calculatedValue = null;
+    return secondValue = null;
+
+  }
+  else updateDisplays('error','error','error');
 };
 
 function clearMemory() {
@@ -32,36 +64,48 @@ function clearMemory() {
   operatorValue = null; 
   displayValue = 0;
   calculatedValue = null;
-  updateDisplay(displayValue);
+  updateDisplays(displayValue,displayValue,'clear');
   };
 
 function updateOperands(event){
   if (!firstValue){
     firstValue = event.target.textContent;
-    updateDisplay(firstValue);
+    updateDisplays(firstValue, firstValue,'push');
   }
   else if (firstValue&&!operatorValue){
     firstValue += event.target.textContent;
-    updateDisplay(firstValue);
+    updateDisplays(firstValue, firstValue,'change');
   }
   else if (firstValue&&operatorValue&&!secondValue){
     secondValue = event.target.textContent;
-    updateDisplay(secondValue);
+    updateDisplays(secondValue,secondValue,'push');
   }
   else if (firstValue&&operatorValue&&secondValue&&!calculatedValue){
     secondValue += event.target.textContent;
-    updateDisplay(secondValue);
+    updateDisplays(secondValue,secondValue,'change');
   }
   else {
-    clearMemory()
-    firstValue = event.target.textContent;
-    updateDisplay(firstValue);
+    // clearMemory()
+    // firstValue = event.target.textContent;
+    updateDisplays('well this is unexpected');
   }
 };
 
-function storeOperator(event){
-  if (firstValue&&!secondValue){
-    operatorValue = event.target.textContent;}
+function runOperator(event){
+  const newOperator = (event.target.textContent ==='=')? operatorValue : event.target.textContent;
+  if (!newOperator&&!firstValue) {
+    return updateDisplays('maybe try a number first, genius','','clear')}
+  else if (!secondValue&&operatorValue){
+    updateAggregator(newOperator,'change');
+    operatorValue = newOperator;
+  }
+  else if (!secondValue&&!operatorValue){
+    updateAggregator(newOperator,'push');
+    operatorValue = newOperator;
+  }
+  else {
+    operatorValue = newOperator;
+    computeValues();}
 };
 
 const add = function(a,b) {
@@ -114,8 +158,11 @@ function divide(a,b) {
     calculatedValue = multiply(a,b);
     break;
   case '/':
-    calculatedValue = divide(a,b);
+    if (b==='0'){calculatedValue = 'nice try, bub'}
+    else {calculatedValue = divide(a,b);}
     break;
+  default:
+    calculatedValue = 'unexpected operation'
   }
   return calculatedValue;
  };
